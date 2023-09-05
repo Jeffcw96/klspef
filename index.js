0089cd691a462caf3e9ad9e0c2b97a9cd1b09b25
Jeffcw96 <changcheewah96@gmail.com>
2023-09-05 20:01:26 +0800

    Add start command

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ app.use(async (ctx) => {
   const password = process.env.PASSWORD;
   const url = `https://appsys.dbkl.gov.my/klspef/aspgateway/logingateway.asp?callback=jQuery1910758393741352775_1693830453821&actiontype=userlogin&usrname=${username}&usrpass=${password}`;
 
-  const result = await fetch(url);
-  ctx.body = JSON.stringify(result);
+  if (ctx.path === "/health") {
+    ctx.status = 200;
+    ctx.body = "OK";
+  } else {
+    const result = await fetch(url);
+    ctx.body = JSON.stringify(result);
+    ctx.status = 500;
+  }
 });
 
 app.listen(3000);
