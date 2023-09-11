771147ae9140cda0e5e0ddc78804a347eb0e15a0
Jeffcw96 <changcheewah96@gmail.com>
2023-09-11 22:14:24 +0800

    Add pino logger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const logger = require("pino")();
+
 const app = new Koa();
 
 app.use(bodyParser({ enableTypes: ["json", "text"] }));
@@ -9,6 +11,7 @@ app.use(async (ctx) => {
   if (ctx.path === "/health") {
     ctx.status = 200;
     console.log("request body", ctx.request.body);
+    logger.info("hello world", ctx.request.body);
     ctx.body = ctx.request.body || "Not body found";
   }
 });
