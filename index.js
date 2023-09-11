9bd3ee374925b9b4c5101f69f748c67bfcaaf556
Jeffcw96 <changcheewah96@gmail.com>
2023-09-11 22:06:58 +0800

    Add koa body parser

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const axios = require("axios");
 const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
 const app = new Koa();
+
+app.use(bodyParser({ enableTypes: ["json", "text"] }));
 
 app.use(async (ctx) => {
   const username = process.env.USERNAME;
   const password = process.env.PASSWORD;
-  const url = `https://appsys.dbkl.gov.my/klspef/aspgateway/logingateway.asp?callback=jQuery1910758393741352775_1693830453821&actiontype=userlogin&usrname=${username}&usrpass=${password}`;
 
   if (ctx.path === "/health") {
     ctx.status = 200;
-    ctx.body = "OK";
-  } else {
-    const result = await axios.get(url);
-    ctx.body = JSON.stringify(result);
-    ctx.status = 500;
+    ctx.body = ctx.request.body || "Not body found";
   }
 });
 
